f6ed8d05626f2d1e9a8c58a8dcc4e7b85ef0b172
akshaya <akshayalnyn@gmail.com>
2023-09-04 22:12:58 +0530

    committed python changes

--- server.py
+++ server.py
@@ -1,19 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+@app.route("/")
+def home():
+    routes = {
+            'Greeting': '/calculator/greeting',
+            'Add': '/calculator/add',
+            'Subtract': '/calculator/subtract'
+            # Add more routes as needed
+        }
+    # Generate HTML with clickable links
+    html = '<h1>Welcome to the Home Page</h1>'
+    html += '<h2>Links to Other Pages:</h2>'
+    html += '<ul>'
+    
+    for title, url in routes.items():
+        html += f'<li><a href="{url}">{title}</a></li>'
+    
+    html += '</ul>'
+    
+    return html
 
-
-@app.route("/calculator/greeting", methods=['GET'])
+@app.route("/calculator/greeting")
 def greeting():
-    return ''
+    return 'hemlu krits'
 
-@app.route("/calculator/add", methods=['POST'])
+@app.route("/calculator/add")
 def add():
-    return ''
+    return 'one plus one'
 
-@app.route("/calculator/subtract", methods=['POST'])
+@app.route("/calculator/subtract")
 def subtract():
-    return ''
+    return str(1-1)
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
